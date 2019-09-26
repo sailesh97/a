3753b90109028cc1c1c3dc063d5ce60c7836ec27
sailesh97 <saileshpatra997@gmail.com>
2019-09-26 20:34:29 +0530

    Landing Navbar & Footer done

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h2>My React App</h2>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <div className="container">
+            <Route path='/register' component={Register} />
+            <Route path='/login' component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
