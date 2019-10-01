4fe62798b44bb03ccc8b0cb3f0d92ef4aab4489c
sailesh97 <saileshpatra997@gmail.com>
2019-10-01 12:18:16 +0530

    Show n Delete Education in Dashboard done

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,6 +6,8 @@ import { getCurrentProfile, deleteAccount } from '../../actions/profileActions';
 import Spinner from '../common/Spinner';
 import ProfileActions from './ProfileActions';
 import Experience from './Experience';
+import Education from './Education';
+
 
 class Dashboard extends Component {
     componentDidMount() {
@@ -39,6 +41,7 @@ class Dashboard extends Component {
                         <p className="lead text-muted">Welcome <Link to={`/profile/${profile.handle}`}>{user.name}</Link></p>
                         <ProfileActions />
                         <Experience experience={profile.experience} />
+                        <Education education={profile.education} />
                         <div style={{ marginBottom: '60px' }} />
                         <button onClick={this.onDeleteClick.bind(this)} className="btn btn-danger">Delete My Account</button>
                     </div>
